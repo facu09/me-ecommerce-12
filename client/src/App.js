13faa7ca0db5797445f8f8ba8db56438a5e5c9a3
facu09 <fcigliuti@gmail.com>
2022-06-05 02:45:03 -0300

    reparao el app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,9 @@ import Router from "./Router/Router";
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className='App'>
       <Router/>      
-=======
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Please Please Please Yeap !!
-        </a>
-      </header>
->>>>>>> 1920c6551eacf186ca5455b95e4b91d1838fda20
+
     </div>
   );
 };

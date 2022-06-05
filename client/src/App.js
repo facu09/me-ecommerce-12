1920c6551eacf186ca5455b95e4b91d1838fda20
facu09 <fcigliuti@gmail.com>
2022-06-04 21:01:16 -0300

    Se arreglo el front servido por el Back a ver si heroku tambien

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Please Please !!
+          Learn React Please Please Please Yeap !!
         </a>
       </header>
     </div>
